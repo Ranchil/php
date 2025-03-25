378be77b901bb688376726f696818fb600a2fbdf
Gustavo Ranchil Silva <130989645+Ranchil@users.noreply.github.com>
2025-03-25 16:46:26 -0300

    php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php require_once 'cabecalho.php'; ?>
+<section class="menu">
+	<div id="logo">
+		<img src="img/fundo.webp" alt="logo do site">
+
+	</div>
+	<div id="botoes">
+		<ul class="nav">
+			<li>
+				<a href="primeira.php" target="janela">Primeira</a>
+			</li>
+			<li>
+				<a href="soma.php" target="janela">Soma</a>
+			</li>
+			<li>
+				<a href="idade.php" target="janela">Idade</a>
+			</li>
+			<li>
+				<a href="compra.php" target="janela">Compra</a>
+			</li>
+		</ul>
+	</div>
+</section>
+<section class="principal">
+	<iframe id="janela" name="janela"></iframe>
+</section>
+<section class="rodape">
+	<div id="endereco">
+		<p>Av. João Manoel dos Santos Ribas, 313</p>
+		<p>Nova Rússia, Ponta Grossa</p>
+		<p>&#9742; (42) 3228-6600</p>
+	</div>
+	<div id="sobre">
+		<p>Página com HTML+CSS+PHP</p>
+	</div>
+</section>
+</body>
+</html>
